6a7a072ad990a3e367657a20ba85abdfc9450a29
twin <550847434@qq.com>
2020-12-29 19:48:55 +0800

    添加了fmcw

--- signalgenerator.py
+++ signalgenerator.py
@@ -2,7 +2,7 @@ from sinsound import cos_wave, FMCW_wave
 import scipy.signal as signal
 import numpy as np
 
-signal_types = ['cos 18kHz', 'cos 20kHz', 'cos 2kHz', 'chirp 40ms 18e3-20e3kHz', 'customized']
+signal_types = ['cos 18kHz', 'cos 20kHz', 'cos 2kHz', 'fmcw 40ms 18e3-20e3kHz', 'customized']
 
 
 def get_all_types():
@@ -17,7 +17,7 @@ def get_signal_by_type(type, customized_signal_file=None):
         return cos_wave(1, 20e3, 48e3, t)
     elif type == 'cos 2kHz':
         return cos_wave(1, 2e3, 48e3, t)
-    elif type == 'chirp 40ms 18e3-20e3kHz':
+    elif type == 'fmcw 40ms 18e3-20e3kHz':
         return FMCW_wave(48e3, t, 0.04, 18e3, 20e3)
     elif type == 'customized':
         return customized_signal_file
