7fa594843c9cfe588dcebead0502be1e2c370b96
twin <550847434@qq.com>
2020-12-29 19:48:38 +0800

    添加了fmcw

--- signalgenerator.py
+++ signalgenerator.py
@@ -1,6 +1,8 @@
-from sinsound import cos_wave
+from sinsound import cos_wave, FMCW_wave
+import scipy.signal as signal
+import numpy as np
 
-signal_types = ['cos 18kHz', 'cos 20kHz', 'cos 2kHz', 'customized']
+signal_types = ['cos 18kHz', 'cos 20kHz', 'cos 2kHz', 'chirp 40ms 18e3-20e3kHz', 'customized']
 
 
 def get_all_types():
@@ -8,12 +10,14 @@ def get_all_types():
 
 
 def get_signal_by_type(type, customized_signal_file=None):
-    t = 60
+    t = 30
     if type == 'cos 18kHz':
         return cos_wave(1, 18e3, 48e3, t)
     elif type == 'cos 20kHz':
         return cos_wave(1, 20e3, 48e3, t)
     elif type == 'cos 2kHz':
         return cos_wave(1, 2e3, 48e3, t)
+    elif type == 'chirp 40ms 18e3-20e3kHz':
+        return FMCW_wave(48e3, t, 0.04, 18e3, 20e3)
     elif type == 'customized':
         return customized_signal_file
--- sinsound.py
+++ sinsound.py
@@ -1,4 +1,5 @@
 import numpy as np
+import scipy.signal as signal
 
 
 def cos_wave(A, f, fs, t, phi=0):
@@ -17,4 +18,14 @@ def cos_wave(A, f, fs, t, phi=0):
     n = np.arange(n)
     # 默认类型为float32
     y = A * np.cos(2 * np.pi * f * n * Ts + phi * (np.pi / 180)).astype(np.float32)
+    return y
+
+
+def FMCW_wave(fs, t, T, f0, f1):
+    ts = np.arange(0, T, 1.0 / fs)
+    sweep = signal.chirp(ts, f0, T, f1, method='linear').astype(np.float32)
+    c = int(t/T)  # 这里取整
+    y = sweep
+    for i in range(c-1):
+        y = np.hstack((y, sweep))
     return y
