238bbfa297ceb658c95d4d4f776fdd1d353f866a
twin <550847434@qq.com>
2021-04-23 11:38:07 +0800

    update

--- signalgenerator.py
+++ signalgenerator.py
@@ -1,6 +1,4 @@
 from sinsound import *
-import scipy.signal as signal
-import numpy as np
 
 signal_types = ['cos 18kHz', 'cos 20kHz',
                 'cos 2kHz', 'fmcw 20ms 18e3-22e3kHz',
@@ -28,8 +26,8 @@ def get_signal_by_type(type, customized_signal_file=None):
         # 有问题
         return FMCW_wave_d_with_sep(48e3, np.arange(0, t, 1 / 48e3), 0.02, 18e3, 22e3, 0)
     elif type == signal_types[6]:
-        # A = [1/3457.0, 1/10384.2, 1/5884.7, 1/5355.7, 1/4112.2, 1/1729.8, 1/1324.4, 1/950.6]
-        A = [1 / 7673.8, 1 / 9956.3, 1 / 4448.2, 1 / 2252.3, 1 / 1780.3, 1 / 724.1, 1 / 407.8, 1 / 265.5]
+        # A = [1/150, 1/70, 1/90, 1/85, 1/68, 1/40, 1/25, 1/14]
+        A = [1, 1, 1, 1, 1, 1, 1, 1]
         alpha = 1 / sum(A)
         print(alpha)
         y = A[0] * cos_wave(1, 17350, 48e3, t)
